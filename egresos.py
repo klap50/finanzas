11e0729ddd7f46a6c23accd03783b2cd93568b6e
Klap <87771561+klap50@users.noreply.github.com>
2023-05-14 22:13:31 -0300

    Update egresos.py

--- egresos.py
+++ egresos.py
@@ -1,56 +1,43 @@
-import mysql.connector
-from database import get_db_connection
+import db  # Cambia 'import database' a 'import db'
 
 def mostrar_egresos():
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute("SELECT id, descripcion, monto FROM egresos")
-    egresos = cursor.fetchall()
+    db_instance = db.Database()
+    egresos = db_instance.query("SELECT id, descripcion, monto FROM egresos")
+    db_instance.close()
     for egreso in egresos:
         print(f"ID: {egreso[0]}, Descripción: {egreso[1]}, Monto: {egreso[2]}")
-    cursor.close()
-    connection.close()
 
-def ingresar_egreso(descripcion, monto):
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    query = "INSERT INTO egresos (descripcion, monto) VALUES (%s, %s)"
-    cursor.execute(query, (descripcion, monto))
-    connection.commit()
-    cursor.close()
-    connection.close()
+def ingresar_egreso():
+    descripcion = input("Ingrese la descripción del egreso: ")
+    monto = float(input("Ingrese el monto del egreso: "))
+    db_instance = db.Database()
+    db_instance.execute("INSERT INTO egresos (descripcion, monto) VALUES (%s, %s)", (descripcion, monto))
+    db_instance.close()
+    print("Egreso ingresado exitosamente.")
 
-def borrar_egreso(id_egreso):
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    query = "DELETE FROM egresos WHERE id = %s"
-    cursor.execute(query, (id_egreso,))
-    connection.commit()
-    cursor.close()
-    connection.close()
+def borrar_egreso():
+    egreso_id = int(input("Ingrese el ID del egreso a borrar: "))
+    db_instance = db.Database()
+    db_instance.execute("DELETE FROM egresos WHERE id = %s", (egreso_id,))
+    db_instance.close()
+    print("Egreso borrado exitosamente.")
 
-def menu():
+def menu_egresos():
     while True:
-        print("Menú de egresos:")
+        print("\nMenú de egresos:")
         print("1. Mostrar egresos")
         print("2. Ingresar egreso")
-        print("3. Borrar egreso por ID")
+        print("3. Borrar egreso")
         print("4. Salir")
-        opcion = input("Seleccione una opción: ")
+        opcion = int(input("Ingrese la opción deseada: "))
 
-        if opcion == "1":
+        if opcion == 1:
             mostrar_egresos()
-        elif opcion == "2":
-            descripcion = input("Ingrese la descripción del egreso: ")
-            monto = float(input("Ingrese el monto del egreso: "))
-            ingresar_egreso(descripcion, monto)
-        elif opcion == "3":
-            id_egreso = int(input("Ingrese el ID del egreso a borrar: "))
-            borrar_egreso(id_egreso)
-        elif opcion == "4":
+        elif opcion == 2:
+            ingresar_egreso()
+        elif opcion == 3:
+            borrar_egreso()
+        elif opcion == 4:
             break
         else:
-            print("Opción no válida, intente nuevamente.")
-
-if __name__ == "__main__":
-    menu()
+            print("Opción inválida. Por favor, ingrese una opción válida.")
